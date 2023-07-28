3651b01c38e0775bc39fb8952f3e352711893c2c
MechelleHernandez <mechelle.hernandez@train.ia.center>
2023-07-27 20:19:02 -0600

    apis

---
+++ APIS/main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+
+app= FastAPI()
+
+@app.get("/")
+def raiz():
+    return {"Hello":"World"}
+
+
+@app.get("/items/{item_id}/{m}")
+def read_item(item_id: int, m: str = None):
+    return {"item_id": item_id, "m":m}
